d9e7b30ecefe7a08ca2e5e4f33548cb708c03358
ShyamAgalcha <156495280+ShyamAgalcha@users.noreply.github.com>
2024-01-13 10:30:22 +0530

    Add files via upload

---
+++ Employee11/demo/src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String designation;
+
+    public Employee(Long id, String name, String designation) {
+        this.id = id;
+        this.name = name;
+        this.designation = designation;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+    // Add other fields as needed
+
+    // Getters and setters
+}
